ffa043d209a58e7d27f84c76a26e18b8c0f9f6c6
Kazutaka Ikemura <banbara.work@gmail.com>
2025-04-23 10:41:39 +0900

    feat: SelectScreen のUIをScaffold追加して整える

--- app/src/main/java/com/yumemi/uwb/sample/ui/SelectScreen.kt
+++ app/src/main/java/com/yumemi/uwb/sample/ui/SelectScreen.kt
@@ -5,7 +5,10 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -13,23 +16,32 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.yumemi.uwb.sample.ui.theme.AndroiduwbsampleTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SelectScreen(
     modifier: Modifier = Modifier,
     onControllerClick: () -> Unit = {},
     onResponderClick: () -> Unit = {},
 ) {
-    Column(
-        modifier = modifier
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        Button(onClick = onControllerClick) {
-            Text("コントローラー")
-        }
-        Spacer(modifier = Modifier.padding(16.dp))
-        Button(onClick = onResponderClick) {
-            Text("レスポンダー")
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            TopAppBar(title = { Text(text = "UWB サンプル アプリ") })
+        },
+    ) { innerPadding ->
+        Column(
+            modifier = modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Button(onClick = onControllerClick) {
+                Text("コントローラー")
+            }
+            Spacer(modifier = Modifier.padding(16.dp))
+            Button(onClick = onResponderClick) {
+                Text("レスポンダー")
+            }
         }
     }
 }
