7adf7e409e64892eb0cb70d69eaf3946eecf3a62
Kazutaka Ikemura <banbara.work@gmail.com>
2025-06-23 11:39:51 +0900

    refactor: BleDeviceConnectionScreenのパッケージ名を変更し、構造を整理

--- app/src/main/java/com/yumemi/uwb/sample/ui/screen/bledeviceconnect/BleDeviceConnectionScreen.kt
+++ app/src/main/java/com/yumemi/uwb/sample/ui/screen/bledeviceconnect/BleDeviceConnectionScreen.kt
@@ -1,4 +1,4 @@
-package com.yumemi.uwb.sample.ui
+package com.yumemi.uwb.sample.ui.screen.bledeviceconnect
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
